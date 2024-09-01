1e0ccb0a30e7344b459d3c2731b679037e4984cc
Sudharshan G <sudharshan1.g@ril.com>
2024-09-01 16:44:14 +0530

    EmptyView Image changed

--- Grocery/Views/EmptyCartView.swift
+++ Grocery/Views/EmptyCartView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct EmptyCartView: View {
     var body: some View {
         VStack(spacing: 24) {
-            Image("emptyCart")
+            
+            Image("nilCart")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
                 .cornerRadius(50)
+                .padding()
             
             Text("Cart is Empty Pleasse Add Items😥🛒 ")
                 .font(.title3)
