bc7915bab5c8a3c6e3d0967e32c75e3ef2e23424
Sudharshan G <sudharshan1.g@ril.com>
2025-07-21 18:45:30 +0530

    bug fixes

--- Grocery/Views/DashboardView.swift
+++ Grocery/Views/DashboardView.swift
@@ -10,27 +10,28 @@ import SwiftUI
 struct DashboardView: View {
     
     @StateObject var dashboardViewModel: DashboardViewModel = DashboardViewModel()
-   
+    
     var body: some View {
         
         
         NavigationStack {
+            
             ScrollView {
-                    
-                    VStack {
-                        
-                        NavBarView()
-                        
-                        CategoryView(viewModel: dashboardViewModel)
-                        
-                        productHorzontalListView
-                            
-                    }
-                    
-                    ProductsListView(viewModel: dashboardViewModel)
-                    
-                }
+                
+                CategoryView(viewModel: dashboardViewModel)
+                
+                productHorzontalListView
+                
+                ProductsListView(viewModel: dashboardViewModel)
+                
+            }
             .background(Color(red: 225 / 255, green: 224 / 255, blue: 238 / 255))
+            .safeAreaInset(edge: .top) {
+                
+                NavBarView()
+                    .background(Color(red: 225 / 255, green: 224 / 255, blue: 238 / 255))
+            }
+            
         }
         
     }
@@ -53,8 +54,8 @@ struct DashboardView: View {
                                     effect
                                         .scaleEffect(phase == .identity ? 1 : 0.3)
                                         .offset(y: phase == .identity ? 0 : 50)
-                                        .rotationEffect(phase == .identity ? Angle(degrees: 0) : Angle(degrees: 45))
-                                        
+//                                        .rotationEffect(phase == .identity ? Angle(degrees: 0) : Angle(degrees: 45))
+                                    
                                 }
                     }
                     .foregroundColor(.black)
